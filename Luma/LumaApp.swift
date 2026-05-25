75fd6890fa37c5ba3173d9a9ab5373dc845730ee
Merlin Kreuzkam <miotares@proton.me>
2026-05-25 23:13:16 +0200

    Initial Commit

---
+++ Luma/LumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumaApp.swift
+//  Luma
+//
+//  Created by Merlin Kreuzkam on 25.05.26.
+//
+
+import SwiftUI
+
+@main
+struct LumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
